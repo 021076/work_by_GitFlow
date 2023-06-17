cc96b367ce44a5381698739ea199ef84abecdc7f
021076 <ro_k_sana@mail.ru>
2023-06-17 15:09:49 +0500

    update str_upp, add str_tit

--- str_func.py
+++ str_func.py
@@ -1,2 +1,13 @@
 def str_upp(user_str):
+    """
+    :param user_str: строка
+    :return: строку в верхнем регистре
+    """
     return user_str.upper()
+
+def str_tit(user_str):
+    """
+    :param user_str: строка
+    :return: строку, каждое слово с заглавной буквы
+    """
+    return user_str.title()
